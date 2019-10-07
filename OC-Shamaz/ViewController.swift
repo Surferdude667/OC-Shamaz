2286b1c5fc2c0cd179ddd49b442b1257820faba2
Bjørn Lau Jørgensen <bjornlau@Bjrns-MacBook-Pro.local>
2019-10-07 08:21:03 +0200

    Initial commit

---
+++ OC-Shamaz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OC-Shamaz
+//
+//  Created by Bjørn Lau Jørgensen on 07/10/2019.
+//  Copyright © 2019 Bjørn Lau Jørgensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
